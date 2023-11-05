284a932ebf921d7014bd48778c62962847c92b44
Said Hammane <said.hammane1@gmail.com>
2023-11-05 09:03:02 +0100

    manage server side

--- server/server.js
+++ server/server.js
@@ -1,30 +1,53 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import {Configuration, OpenAIApi} from 'openai';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
 
 dotenv.config();
 
-const configuration = new Configuration({
-    apiKey: process.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello from Me',
-    })
-})
-
-app.post('/', async (req, res) => {
-    try{
-        const prompt = req.body.prompt;
-    }catch(error){
-        
-    }
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Me",
+  });
 });
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    openai.Completion.create(
+      {
+        engine: "text-davinci-002",
+        prompt: `${prompt}`,
+        max_tokens: 3000,
+      },
+      function (error, response) {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Something went wrong");
+        } else {
+          const botResponse = response.choices[0].text;
+          console.log(botResponse);
+          res.status(200).send({
+            bot: botResponse,
+          });
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
